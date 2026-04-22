969531819047cfb3a932955abff2c9593416e8a6
Rıza Emre ARAS <r.emrearas@proton.me>
2026-04-22 20:56:45 +0300

    Cila: Apply brew style auto-fixes to phantom-wg-mac cask

--- Casks/phantom-wg-mac.rb
+++ Casks/phantom-wg-mac.rb
@@ -1,19 +1,22 @@
+# frozen_string_literal: true
+
 cask "phantom-wg-mac" do
   version "1.2.1"
 
   on_arm do
     sha256 "a3f2eb72c9f876cea17c32afaea0c0c2d82e609d187d56ab18c3aaf3a9dd621c"
+
     url "https://github.com/ARAS-Workspace/phantom-wg/releases/download/mac-v#{version}/Phantom-WG-MacOS-#{version}-arm64.dmg"
   end
-
   on_intel do
     sha256 "95bdc0be8cbabd4d5b278883d990cb9e3ed87a96272045c7574c31e224379405"
+
     url "https://github.com/ARAS-Workspace/phantom-wg/releases/download/mac-v#{version}/Phantom-WG-MacOS-#{version}-x86_64.dmg"
   end
 
   name "Phantom-WG Mac"
   desc "Censorship-resistant WireGuard VPN client with Ghost Mode and Split-Tunneling"
-  homepage "https://www.phantom.tc"
+  homepage "https://www.phantom.tc/"
 
   livecheck do
     url "https://github.com/ARAS-Workspace/phantom-wg/releases.atom"
@@ -28,8 +31,8 @@ cask "phantom-wg-mac" do
 
   zap trash: [
     "~/Library/Containers/com.remrearas.Phantom-WG-MacOS",
-    "~/Library/Containers/com.remrearas.Phantom-WG-MacOS.PhantomTunnel",
     "~/Library/Containers/com.remrearas.Phantom-WG-MacOS.PhantomSplitTunnel",
+    "~/Library/Containers/com.remrearas.Phantom-WG-MacOS.PhantomTunnel",
     "~/Library/Group Containers/group.com.remrearas.phantom-wg-macos",
   ]
 
