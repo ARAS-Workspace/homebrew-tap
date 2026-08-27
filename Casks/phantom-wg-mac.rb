02a20b90a3777634c216a3e303718e5ea3c8b0d4
Rıza Emre ARAS <r.emrearas@proton.me>
2026-08-27 21:26:46 +0300

    Bump: phantom-wg-mac 2.1.1 → 2.1.2

--- Casks/phantom-wg-mac.rb
+++ Casks/phantom-wg-mac.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 cask "phantom-wg-mac" do
-  version "2.1.1"
+  version "2.1.2"
 
   on_arm do
-    sha256 "d3055fedbcf8fe01c0e5b0e819ce00098f19f405aa1495e92fda82759a216cfc"
+    sha256 "e24e4f9109dc4b1b83e1a3485100b93cb55f7d5c37468ac60b750a31b8167c61"
 
     url "https://github.com/ARAS-Workspace/phantom-wg/releases/download/mac-v#{version}/Phantom-WG-MacOS-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "e88efeae5dec261ff4be1d458e5c295b8b3494c21d2ca874b2b9081417d58fcd"
+    sha256 "c9fe6dfc625f8328284172d2c47031d451e74e1dd09c581533248f743450eb7d"
 
     url "https://github.com/ARAS-Workspace/phantom-wg/releases/download/mac-v#{version}/Phantom-WG-MacOS-#{version}-x86_64.dmg"
   end
